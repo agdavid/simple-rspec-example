ee19c0c51a7786bc2e4b848ba5abe70313180d4e
agdavid <antonio.g.david@gmail.com>
2016-10-19 15:47:35 -0400

    use YAML to create Library instance in specs

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,6 +1,26 @@
 require 'spec_helper'
 
-describe Book do 
+describe "Library object" do
 
+    before(:all) do 
+    
+        lib_obj = [
+            Book.new("JavaScript: The Good Parts", "Douglas Crockford", :development),
+            Book.new("Designing with Web Standards", "Jeffrey Zeldman", :design),
+            Book.new("Don't Make me Think", "Steve Krug", :usability),
+            Book.new("JavaScript Patterns", "Stoyan Stefanov", :development),
+            Book.new("Responsive Web Design", "Ethan Marcotte", :design)
+        ]
+
+        # open the file 'books.yml' in 'w'rite mode and dump the books into the file
+        File.open "books.yml", "w" do |f|
+            f.write YAML::dump lib_obj
+        end
+    end
+
+    # before each test create a Library instance with the yaml file
+    before(:each) do 
+        @lib = Library.new("books.yml")
+    end
 
 end
