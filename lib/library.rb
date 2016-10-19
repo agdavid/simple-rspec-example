51791a639fb3e0ff9428c3c123d206f01bf7253e
agdavid <antonio.g.david@gmail.com>
2016-10-19 16:11:12 -0400

    add code to Library to pass initialize specs

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,10 @@
 class Library
 
+  attr_accessor :books
+
+  def initialize(lib_file=false)
+      @lib_file = lib_file
+      @books = @lib_file ? YAML::load(File.read(@lib_file)) : []
+  end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -27,13 +27,13 @@ describe "Library object" do
         context "with no parameters" do 
             it "has no books" do 
                 lib = Library.new
-                expect(lib.length).to eq(0)
+                expect(lib.books.length).to eq(0)
             end
         end
 
         context "with a yaml file parameter" do 
             it "has five books" do 
-                expect(@lib.length).to eq(5)
+                expect(@lib.books.length).to eq(5)
             end
         end
 
