ebe46a344fe1f1bd5bf780b78f2dab64693494ad
agdavid <antonio.g.david@gmail.com>
2016-10-19 16:08:14 -0400

    add more specs to add book, get book, save library

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -36,6 +36,36 @@ describe "Library object" do
                 expect(@lib.length).to eq(5)
             end
         end
+
+    end
+
+    describe "#get_books_in_category" do 
+        it "should return all the books in a given category" do 
+            expect(@lib.get_books_in_category(:development).length).to eq(2)
+        end
+    end
+
+    describe "#add_book" do
+        before (:each) do 
+            original_book_count = Book.all.count
+            new_book = Book.new("Designing for the Web", "Mark Boulton", :design)
+            @lib.add_book(new_book)
+            new_book_count = Book.all.count
+        end 
+        it "should create an instance of Book" do
+            expect(original_book_count - new_book_count).to eq(1)
+            expect(@lib.get_book("Designing for the Web")).to be_an_instance_of(Book)
+        end
+    end
+
+    describe "#save" do 
+        it "should save the books to the library" do 
+            books = @lib.books.collect { |book| book.title }
+            @lib.save
+            lib2 = Library.new("books.yml")
+            books2 = lib2.books.collect { |book| book.title }
+            expect(books).to eq(books2)
+        end
     end
 
 end
